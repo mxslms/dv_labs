34665102fe2b020c01768247a72871670f79849c
mxslms <mkonwsh@gmail.com>
2025-04-20 07:54:33 -0400

    mod 1 done

--- dv lab 1.py
+++ dv lab 1.py
@@ -1,5 +1,37 @@
 #dv lab 1
-import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-plt.plot(5,5, 'x')
-plt.show
+df_can = pd.read_excel(
+    'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Canada.xlsx',
+    sheet_name='Canada by Citizenship',
+    skiprows=range(20),
+    skipfooter=2)
+
+print('Data read into a pandas dataframe!')
+
+df_can.drop(['AREA','REG','DEV','Type','Coverage'], axis=1, inplace=True)
+df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent', 'RegName':'Region'}, inplace=True)
+
+df_can['Total'] = df_can.sum(axis=1, numeric_only=True)
+
+df_can.head()
+
+
+df_can.set_index('Country', inplace=True)
+df_can.index.name = None
+    
+df_can.columns = list(map(str, df_can.columns))
+
+years = list(map(str, range(1980, 2014)))
+
+condition = df_can.loc[:,'1980':]==16
+#condition = df_can['1980']==16
+print(condition)
+df_can[condition]
+
+#show any columns with a number == to somethign i want
+df_can[(df_can.loc[:,'1980':'2013']>22000).any(axis=1)]
+
+df_can.sort_values(by='Total', ascending=False, axis=0, inplace=True)
+df_can.head(5)
---
+++ dv lab2.py
@@ -0,0 +1,68 @@
+#lab2
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+df_can = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Canada.csv')
+
+df_can.head()
+
+df_can.set_index('Country', inplace=True)
+df_can.index.name = None
+years = list(map(str, range(1980, 2014)))
+
+haiti = df_can.loc['Haiti', years] # passing in years 1980 - 2013 to exclude the 'total' column
+haiti.index = haiti.index.map(int) 
+haiti.plot(kind='line')
+
+plt.title('Immigration from Haiti')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+# annotate the 2010 Earthquake. 
+# syntax: plt.text(x, y, label)
+plt.text(2000, 6000, '2010 Earthquake') # see note below
+
+plt.show() 
+
+CandI = df_can.loc[['China','India'],years]
+CandI = CandI.transpose()
+CandI.plot(kind='line')
+CandI.index = CandI.index.map(int)
+plt.title('Immigrants from China and India')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+df_top5 = df_can.nlargest(5, 'Total')
+df_top5 = df_top5[years].transpose()
+df_top5.index = df_top5.index.map(int)
+df_top5.plot(kind='line', figsize=(14, 8))
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+
+plt.show()
+
+        
+           
+           
+           
+           
+           
+           
+        
+        
+
+
+
+
+
+
+
+
+
+
+
