9aae7d9dde001ee3feefa093583342225b5f6732
mxslms <mkonwsh@gmail.com>
2025-05-03 07:36:54 -0400

    other file

---
+++ dv_day2.py
@@ -0,0 +1,72 @@
+#Area Plots, Histograms, and Bar Charts
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.style.use('ggplot')
+
+#get data
+df_can = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Canada.csv')
+print('Data read into a pandas dataframe!')
+
+#preprocess data
+df_can.set_index('Country', inplace=True)
+df_can.head()
+print('data dimensions:', df_can.shape)
+years = list(map(str, range(1980, 2014)))
+
+#AREA PLOTS
+#top 5
+df_can.sort_values(['Total'], ascending=False, axis=0, inplace=True)
+df_top5 = df_can.head()
+df_top5 = df_top5[years].transpose()
+df_top5.head()
+
+# let's change the index values of df_top5 to type integer for plotting
+df_top5.index = df_top5.index.map(int)
+df_top5.plot(kind='area',
+             stacked=False,
+             alpha=0.4,
+             figsize=(20, 10))  # pass a tuple (x, y) size
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+# option 2: preferred option with more flexibility
+ax = df_top5.plot(kind='area', stacked=False, alpha=0.35, figsize=(20, 10))
+
+ax.set_title('Immigration Trend of Top 5 Countries')
+ax.set_ylabel('Number of Immigrants')
+ax.set_xlabel('Years')
+
+#HISTOGRAMS
+#Question: What is the frequency distribution of the number (population) of new immigrants from the various countries to Canada in 2013?
+df_can['2013'].head()
+
+# np.histogram returns 2 values
+count, bin_edges = np.histogram(df_can['2013'])
+print(count) # frequency count
+print(bin_edges) # bin ranges, default = 10 bins
+
+df_can['2013'].plot(kind='hist', figsize=(8, 5))
+plt.title('Histogram of Immigration from 195 Countries in 2013')
+plt.ylabel('Number of Countries')
+plt.xlabel('Number of Immigrants')
+plt.show()
+
+# 'bin_edges' is a list of bin intervals
+count, bin_edges = np.histogram(df_can['2013'])
+df_can['2013'].plot(kind='hist', figsize=(8, 5), xticks=bin_edges)
+plt.title('Histogram of Immigration from 195 countries in 2013') # add a title to the histogram
+plt.ylabel('Number of Countries') # add y-label
+plt.xlabel('Number of Immigrants') # add x-label
+plt.show()
+
+#Question: What is the immigration distribution for Denmark, Norway, and Sweden for years 1980 - 2013?
+df_can.loc[['Denmark', 'Norway', 'Sweden'], years]
+df_t = df_can.loc[['Denmark', 'Norway', 'Sweden'], years].transpose()
+df_t.plot(kind='hist', figsize=(10, 6))
+plt.title('Histogram of Immigration from Denmark, Norway, and Sweden from 1980 - 2013')
+plt.ylabel('Number of Years')
+plt.xlabel('Number of Immigrants')
+plt.show()
