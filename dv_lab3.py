07b69c976ba0d1953f3f72dc7e5de1e84b903e69
mxslms <mkonwsh@gmail.com>
2025-05-17 07:17:23 -0400

    missed file

--- dv_lab3.py
+++ dv_lab3.py
@@ -1,1 +1,218 @@
-#lab for mod 3
+#lab for mod 3import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches # needed for waffle Charts
+
+#Import Primary Modules:
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+from PIL import Image # converting images into arrays
+
+#import seaborn
+import seaborn as sns
+
+#import wordcloud
+import wordcloud
+
+df_can = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Canada.csv')
+#set Country as index
+df_can.set_index('Country', inplace=True)
+
+# let's create a new dataframe for these three countries 
+df_dsn = df_can.loc[['Denmark', 'Norway', 'Sweden'], :]
+
+# let's take a look at our dataframe
+df_dsn
+
+# compute the proportion of each category with respect to the total
+total_values = df_dsn['Total'].sum()
+category_proportions = df_dsn['Total'] / total_values
+
+# print out proportions
+pd.DataFrame({"Category Proportion": category_proportions})
+
+width = 40 # width of chart
+height = 10 # height of chart
+
+total_num_tiles = width * height # total number of tiles
+
+print(f'Total number of tiles is {total_num_tiles}.')
+
+# compute the number of tiles for each category
+tiles_per_category = (category_proportions * total_num_tiles).round().astype(int)
+
+# print out number of tiles per category
+pd.DataFrame({"Number of tiles": tiles_per_category})
+
+# initialize the waffle chart as an empty matrix
+waffle_chart = np.zeros((height, width), dtype = np.uint)
+
+# define indices to loop through waffle chart
+category_index = 0
+tile_index = 0
+
+# populate the waffle chart
+for col in range(width):
+    for row in range(height):
+        tile_index += 1
+
+        # if the number of tiles populated for the current category is equal to its corresponding allocated tiles...
+        if tile_index > sum(tiles_per_category[0:category_index]):
+            # ...proceed to the next category
+            category_index += 1       
+            
+        # set the class value to an integer, which increases with class
+        waffle_chart[row, col] = category_index
+        
+print ('Waffle chart populated!')
+
+waffle_chart
+
+# instantiate a new figure object
+fig = plt.figure()
+
+# use matshow to display the waffle chart
+colormap = plt.cm.coolwarm
+plt.matshow(waffle_chart, cmap=colormap)
+plt.colorbar()
+plt.show()
+
+# instantiate a new figure object
+fig = plt.figure()
+
+# use matshow to display the waffle chart
+colormap = plt.cm.coolwarm
+plt.matshow(waffle_chart, cmap=colormap)
+plt.colorbar()
+
+# get the axis
+ax = plt.gca()
+
+# set minor ticks
+ax.set_xticks(np.arange(-.5, (width), 1), minor=True)
+ax.set_yticks(np.arange(-.5, (height), 1), minor=True)
+    
+# add gridlines based on minor ticks
+ax.grid(which='minor', color='w', linestyle='-', linewidth=2)
+
+plt.xticks([])
+plt.yticks([])
+plt.show()
+
+# instantiate a new figure object
+fig = plt.figure()
+
+# use matshow to display the waffle chart
+colormap = plt.cm.coolwarm
+plt.matshow(waffle_chart, cmap=colormap)
+plt.colorbar()
+
+# get the axis
+ax = plt.gca()
+
+# set minor ticks
+ax.set_xticks(np.arange(-.5, (width), 1), minor=True)
+ax.set_yticks(np.arange(-.5, (height), 1), minor=True)
+    
+# add gridlines based on minor ticks
+ax.grid(which='minor', color='w', linestyle='-', linewidth=2)
+
+plt.xticks([])
+plt.yticks([])
+
+# compute cumulative sum of individual categories to match color schemes between chart and legend
+values_cumsum = np.cumsum(df_dsn['Total'])
+total_values = values_cumsum[len(values_cumsum) - 1]
+
+# create legend
+legend_handles = []
+for i, category in enumerate(df_dsn.index.values):
+    label_str = category + ' (' + str(df_dsn['Total'][i]) + ')'
+    color_val = colormap(float(values_cumsum[i])/total_values)
+    legend_handles.append(mpatches.Patch(color=color_val, label=label_str))
+
+# add legend to chart
+plt.legend(handles=legend_handles,
+           loc='lower center', 
+           ncol=len(df_dsn.index.values),
+           bbox_to_anchor=(0., -0.2, 0.95, .1)
+          )
+plt.show()
+
+
+from pywaffle import Waffle
+
+#Set up the Waffle chart figure
+
+fig = plt.figure(FigureClass = Waffle,
+                 rows = 20, columns = 30, #pass the number of rows and columns for the waffle 
+                 values = df_dsn['Total'], #pass the data to be used for display
+                 cmap_name = 'tab20', #color scheme
+                 legend = {'labels': [f"{k} ({v})" for k, v in zip(df_dsn.index.values,df_dsn.Total)],
+                            'loc': 'lower left', 'bbox_to_anchor':(0,-0.1),'ncol': 3}
+                 #notice the use of list comprehension for creating labels 
+                 #from index and total of the dataset
+                )
+
+#Display the waffle chart
+plt.show()
+
+
+df_ci = df_can.loc[['China', 'India'], :]
+fig = plt.figure(FigureClass = Waffle,
+                 rows = 10, columns = 20, #pass the number of rows and columns for the waffle 
+                 values = df_ci['Total'], #pass the data to be used for display
+                 cmap_name = 'tab20', #color scheme
+                 legend = {'labels': [f"{k} ({v})" for k, v in zip(df_dsn.index.values,df_dsn.Total)],
+                            'loc': 'lower left', 'bbox_to_anchor':(0,-0.1),'ncol': 3}
+                 #notice the use of list comprehension for creating labels 
+                 #from index and total of the dataset
+                )
+
+#Display the waffle chart
+plt.show()
+
+df_can = df_can.reset_index()
+print(df_can.columns)
+df_can['Country'].nunique()
+print(df_can.shape)
+df_can.describe()
+df_ci.head(5)
+
+
+from wordcloud import WordCloud, STOPWORDS
+
+import urllib
+
+# # open the file and read it into a variable alice_novel
+alice_novel = urllib.request.urlopen('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/alice_novel.txt').read().decode("utf-8")
+
+stopwords = set(STOPWORDS)
+# instantiate a word cloud object
+stopwords.add('said') # add the words said to stopwords
+alice_wc = WordCloud(stopwords=stopwords)
+
+# re-generate the word cloud
+alice_wc.generate(alice_novel)
+
+# display the cloud
+fig = plt.figure(figsize=(14, 18))
+
+plt.imshow(alice_wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+#save mask to alice_mask
+alice_mask = np.array(Image.open(urllib.request.urlopen('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/labs/Module%204/images/alice_mask.png')))
+# instantiate a word cloud object
+alice_wc = WordCloud(background_color='white', max_words=2000, mask=alice_mask, stopwords=stopwords)
+
+# generate the word cloud
+alice_wc.generate(alice_novel)
+
+# display the word cloud
+fig = plt.figure(figsize=(14, 18))
+
+plt.imshow(alice_wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
